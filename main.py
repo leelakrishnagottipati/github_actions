716954f532953b89068fa494e93e4b2b743c3019
Sagar <skpanda256@gmail.com>
2021-08-17 10:16:07 +0530

    changed main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-print("FNAME")
-print("LNAME")
+print(os.environ.get('first_name'))
+print(os.environ.get('last_name'))
 print((os.environ.get('first_name'))=='sagar')
 print((os.environ.get('last_name'))=='panda')
