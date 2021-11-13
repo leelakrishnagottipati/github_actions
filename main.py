bcde847b70596b2c33597d63be43ad2b48e63c30
leelakrishnagottipati <92711134+leelakrishnagottipati@users.noreply.github.com>
2021-11-13 10:06:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import os
 print(os.environ.get('first_name'))
 print(os.environ.get('last_name'))
 print((os.environ.get('first_name'))=='Sagar')
-print((os.environ.get('last_name'))=='p')
+print((os.environ.get('last_name'))=='krishna')
